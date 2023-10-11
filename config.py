1a276a30c6943e0d876afe3b586789c42ce2622c
AmitVermaXD <131673747+AmitVermaXD@users.noreply.github.com>
2023-10-11 12:00:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("27890167"))
+API_HASH = getenv("f31f6f41da08fc767c626931eade82c9")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("6670342404:AAHwMhGtlwGdl7ys4vcF_w03jviSgO7EYxM")
+OWNER_ID = int(getenv("6227452199"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
+MONGO_DB_URI = getenv("mongodb+srv://kaushalxraja:JSck0@cluster0.dfvemtb.mongodb.net/?retryWrites=true&w=majority")
 MUST_JOIN = getenv("MUST_JOIN", None)
